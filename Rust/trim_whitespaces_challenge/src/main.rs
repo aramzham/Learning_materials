3a940d46cf55688d6a30b15bc911761d7b26cda1
Aram Zhamkochyan <aramzham@hotmail.com>
2023-04-19 23:27:03 +0200

    enhanced solution to remove all sorts of whitespaces

--- Rust/trim_whitespaces_challenge/src/main.rs
+++ Rust/trim_whitespaces_challenge/src/main.rs
@@ -1,15 +1,21 @@
 fn main() {
     let test1 = "We need more space.";
     assert_eq!(trim_spaces(test1), "We need more space.");
-    
+
     let test2 = "   There's space in front";
     assert_eq!(trim_spaces(&test2), "There's space in front");
-    
+
     let test3 = "There's space to the rear. ";
     assert_eq!(trim_spaces(&test3[..]), "There's space to the rear.");
 
     let test4 = "   Surrounded by space...     ";
     assert_eq!(trim_spaces(&test4[..]), "Surrounded by space...");
+
+    let test5 = "\n\r starting with whitespaces";
+    assert_eq!(trim_spaces(test5), "starting with whitespaces");
+    
+    let test6 = "\t   whitespaces everywhere \t\n\r   ";
+    assert_eq!(trim_spaces(test6), "whitespaces everywhere");
     
     println!("tests passed!!");
 }
@@ -17,16 +23,17 @@ fn main() {
 fn trim_spaces(input: &str) -> &str {
     let mut first_letter_index = 0;
     let mut last_letter_index = input.len() - 1;
+    let whitespaces = [' ', '\t', '\n', '\r'];
 
     for (index, letter) in input.chars().enumerate() {
-        if letter != ' ' {
+        if whitespaces.iter().all(|&char| char != letter) {
             first_letter_index = index;
             break;
         }
     }
 
     for (index, letter) in input.chars().rev().enumerate() {
-        if letter != ' ' {
+        if whitespaces.iter().all(|&char| char != letter) {
             last_letter_index = input.len() - index;
             break;
         }
