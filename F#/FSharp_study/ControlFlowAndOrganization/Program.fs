3295de69edc816597c9d1ebed0be87ac0702879c
Aram Zhamkochyan <aramzham@hotmail.com>
2023-08-09 00:08:43 +0200

    demo - user input loop

--- F#/FSharp_study/ControlFlowAndOrganization/Program.fs
+++ F#/FSharp_study/ControlFlowAndOrganization/Program.fs
@@ -1,2 +1,19 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let promptUser() =
+    Console.Write("(d)eposit, (w)ithdraw or e(x)it: ")
+    Console.ReadLine() // as return
+    
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Hello from the transaction processor!")
+    
+    let mutable isRunning = true
+    while isRunning do
+        let action = promptUser()
+        printfn $"You told me to do this: %s{action}"
+        
+        isRunning <- action <> "x"
+    
+    Console.WriteLine("Bye!!")
+    0 // must return something
