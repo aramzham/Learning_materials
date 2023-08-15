09d50ede52342f939aac4c26f047d069338bab5e
Aram Zhamkochyan <aramzham@hotmail.com>
2023-08-15 23:23:42 +0200

    synopses added

---
+++ F#/FSharp_study/ControlFlowAndOrganization/Driver.fs
@@ -0,0 +1,30 @@
+﻿module Transactions.Driver
+
+open System
+
+module UserConsole =
+    let private promptUser() =
+        Console.Write("(d)eposit, (w)ithdraw or e(x)it: ")
+        Console.ReadLine() // as return
+    
+    let private getAmount () =
+        Console.Write("Enter the amount of the transaction: ")
+        Console.ReadLine() |> Decimal.Parse
+        
+    let userLoop () =
+        let mutable balance = 0m
+    
+        let mutable isRunning = true
+        while isRunning do
+            printfn $"balance: %f{balance}"
+            
+            let action = promptUser()
+            printfn $"You told me to do this: %s{action}"
+            
+            balance <-
+                match action with
+                | "d" -> balance + getAmount() 
+                | "w" -> balance - getAmount() 
+                | _ ->
+                    isRunning <- action <> "x"
+                    balance
--- F#/FSharp_study/ControlFlowAndOrganization/Program.fs
+++ F#/FSharp_study/ControlFlowAndOrganization/Program.fs
@@ -1,33 +1,10 @@
 ﻿open System
 
-let promptUser() =
-    Console.Write("(d)eposit, (w)ithdraw or e(x)it: ")
-    Console.ReadLine() // as return
-    
-let getAmount () =
-    Console.Write("Enter the amount of the transaction: ")
-    Console.ReadLine() |> Decimal.Parse
-    
 [<EntryPoint>]
 let main argv =
     Console.WriteLine("Hello from the transaction processor!")
     
-    let mutable balance = 0m
-    
-    let mutable isRunning = true
-    while isRunning do
-        printfn $"balance: %f{balance}"
-        
-        let action = promptUser()
-        printfn $"You told me to do this: %s{action}"
-        
-        balance <-
-            match action with
-            | "d" -> balance + getAmount() 
-            | "w" -> balance - getAmount() 
-            | _ ->
-                isRunning <- action <> "x"
-                balance
+    Transactions.Driver.UserConsole.userLoop()
     
     Console.WriteLine("Bye!!")
     0 // must return something
