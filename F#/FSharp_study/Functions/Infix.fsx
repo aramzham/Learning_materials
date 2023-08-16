10c0196c5a54e8ef83320148c6408bdd730a0375
Aram Zhamkochyan <aramzham@hotmail.com>
2023-08-16 23:43:11 +0200

    functions started

---
+++ F#/FSharp_study/Functions/Infix.fsx
@@ -0,0 +1,10 @@
+﻿printfn "%i" ((+) ((+) 1 2) 3)
+
+let add1 = (+) 1 // partial application
+// here add1 is a function int -> int
+
+printfn "%i" (add1 5)
+
+let (|<>|) x y = if x>y then x-y else y-x
+
+printfn "%d" (9 |<>| 3 |<>| 0)
