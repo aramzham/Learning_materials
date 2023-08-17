14f5f43968e6a3990b66491813e1968a75f67885
Aram Zhamkochyan <aramzham@hotmail.com>
2023-08-18 00:26:45 +0200

    function composition

---
+++ F#/FSharp_study/Functions/FunctionComposition.fsx
@@ -0,0 +1,6 @@
+﻿let f x = x * x
+let g x = x + 1
+printfn "%i" (g (f 4))
+
+let g_f x = g(f(x))
+printfn "%d" (g_f 3)
---
+++ F#/FSharp_study/Functions/PartialApplication.fsx
@@ -0,0 +1,6 @@
+﻿let infixFoldSum = List.fold (+) 0 [1..100]
+printfn "%d" infixFoldSum
+
+// partially apply fold
+let sum = List.fold (+) 0
+printfn "%i" (sum [1..100])
