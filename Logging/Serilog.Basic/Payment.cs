4aecfc17fb31cd054d76a1d31eea775a938cfaad
Aram Zhamkochyan <aram532@yandex.ru>
2024-12-13 23:44:12 +0400

    timed operation using serilog

--- Logging/Serilog.Basic/Payment.cs
+++ Logging/Serilog.Basic/Payment.cs
@@ -2,6 +2,7 @@
 
 public class Payment
 {
+    public Guid Id { get; set; }
     public int PaymentType { get; set; }
     public decimal Amount { get; set; }
     public DateTime OccuredAt { get; set; }
--- Logging/Serilog.Basic/Program.cs
+++ Logging/Serilog.Basic/Program.cs
@@ -2,19 +2,22 @@
 using Serilog.Basic;
 using Serilog.Context;
 using Serilog.Formatting.Json;
+using SerilogTimings.Extensions;
 
 var logger = new LoggerConfiguration()
-    .WriteTo.Console(new JsonFormatter())
+    // .WriteTo.Console(new JsonFormatter())
+    .WriteTo.Console()
     .WriteTo.File("log.txt")
     .Enrich.FromLogContext() // to enrich the log context with the properties we want to expose
-    .Destructure.ByTransforming<Payment>(p => new { p.Amount, p.PaymentType }) // to omit the properties we don't want to expose
+    .Destructure
+    .ByTransforming<Payment>(p => new { p.Amount, p.PaymentType }) // to omit the properties we don't want to expose
     .CreateLogger();
 
 logger.Information("Hello, World!");
 
 Log.Logger = logger;
 
-var payment = new Payment() { PaymentType = 2, Amount = 300m, OccuredAt = DateTime.UnixEpoch };
+var payment = new Payment() { PaymentType = 2, Amount = 300m, OccuredAt = DateTime.UnixEpoch, Id = Guid.NewGuid() };
 var dict = new Dictionary<int, string>()
 {
     { 1, "One" },
@@ -32,6 +35,23 @@ using (LogContext.PushProperty("Time", TimeOnly.FromDateTime(DateTime.Now)))
     logger.Information("Dictionary data = {Dictionary}", dict);
     // to print out the type of dictionary use $ sign
     logger.Information("Dictionary data = {$Dictionary}", dict);
+}
+
+using (logger.TimeOperation("the payment with id {PaymentId} has been processed", payment.Id))
+{
+    logger.Information("Processing payment");
+    Thread.Sleep(1000);
+}
+
+var operation = logger.BeginOperation("the payment with id {PaymentId} has been processed", payment.Id);
+try
+{
+    logger.Information("Processing payment");
+    Thread.Sleep(1000);
+}
+finally
+{
+    operation.Complete();
 }
 
 await Log.CloseAndFlushAsync();
