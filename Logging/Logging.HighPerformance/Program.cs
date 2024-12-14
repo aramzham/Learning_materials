1fcb6a5f793899ab4942b45739a1851da9b21385
Aram Zhamkochyan <aram532@yandex.ru>
2024-12-15 00:18:21 +0400

    high performance project

---
+++ Logging/Logging.HighPerformance/Program.cs
@@ -0,0 +1,16 @@
+﻿using Logging.HighPerformance;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(x =>
+    {
+        x.ClearProviders();
+        x.AddConsole();
+    })
+    .ConfigureServices((context, services) => { services.AddSingleton<PaymentService>(); })
+    .Build();
+
+var paymentService = host.Services.GetRequiredService<PaymentService>();
+paymentService.Create("some@email.com", 13.3m, 10);
