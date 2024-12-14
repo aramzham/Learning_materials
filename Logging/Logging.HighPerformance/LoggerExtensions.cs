0d8c1247bd210203f60a3d031638711c6cc1c756
Aram Zhamkochyan <aram532@yandex.ru>
2024-12-14 11:18:08 +0400

    before source generated logging

---
+++ Logging/Logging.HighPerformance/LoggerExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Logging.HighPerformance;
+
+public static partial class LoggerExtensions
+{
+    [LoggerMessage(EventId = 1,
+        Level = LogLevel.Information,
+        Message = "Payment created for user {email} for product {productId} with amount {amount}")]
+    public static partial void LogPaymentCreation(this ILogger logger, string email, decimal amount, int productId);
+}
---
+++ Logging/Logging.HighPerformance/PaymentService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Logging.HighPerformance;
+
+public class PaymentService
+{
+    // using this you'll avoid boxing of log message parameters and the additional allocation of the parameters array
+    private static Action<ILogger, string, int, decimal, Exception?> _logCreate =
+        LoggerMessage.Define<string, int, decimal>(
+            LogLevel.Information,
+            new EventId(1, nameof(Create)),
+            "Customer {Email} purchased product {ProductId} at {Amount}");
+
+    private readonly ILogger<PaymentService> _logger;
+
+    public PaymentService(ILogger<PaymentService> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Create(string email, decimal amount, int productId)
+    {
+        // do work
+        // _logger.LogInformation("Customer {Email} purchased product {ProductId} at {Amount}", email, productId, amount);
+        _logCreate(_logger, email, productId, amount, null);
+    }
+}
