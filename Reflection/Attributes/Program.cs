9c576fe805e085361a927e78ac0182e5f8980005
Aram Zhamkochyan <aramzham@hotmail.com>
2025-02-25 08:41:10 +0400

    reflection: restrict attributes

---
+++ Reflection/Attributes/Program.cs
@@ -0,0 +1,27 @@
+﻿[AttributeUsage(AttributeTargets.Class)]
+public sealed class CustomClassAttribute : Attribute
+{
+    public CustomClassAttribute(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class CustomMethodAttribute : Attribute;
+
+[AttributeUsage(AttributeTargets.Parameter)]
+public sealed class CustomParameterAttribute : Attribute;
+
+[CustomClass("OurClass")]
+public class OurDecoratedClass
+{
+    // [CustomClass("OurMethod")] // this won't work!!
+    [CustomMethod]
+    public void OurDecoratedMethod([CustomParameter] int parameter)
+    {
+        Console.WriteLine("Hello from decorated method");
+    }
+}
