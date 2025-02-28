f99a72a725007cbaecbf7a0ef609d828a941076e
Aram Zhamkochyan <aramzham@hotmail.com>
2025-02-28 08:52:49 +0400

    reflection: generics

---
+++ Reflection/Generics/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using Generics;
+
+var genericTypeA = typeof(GenericTypeA<,,>);
+
+var genericTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsGenericType);
+
+Console.WriteLine("The generic types in this assembly are:");
+foreach (var type in genericTypes)
+{
+    Console.WriteLine(type.Name);
+
+    Console.WriteLine("\tType parameters:");
+    foreach (var typeParameter in type.GetGenericArguments())
+    {
+        Console.WriteLine($"\t\t{typeParameter.Name}");
+        
+        var constraints = typeParameter.GetGenericParameterConstraints();
+        if (constraints.Length > 0)
+        {
+            Console.WriteLine("\t\t\tConstraints:");
+            foreach (var constraint in constraints)
+            {
+                Console.WriteLine($"\t\t\t\t must be {constraint.Name}");
+            }
+        }
+    }
+
+    Console.WriteLine("\tMembers:");
+    foreach (var member in type.GetMembers())
+    {
+        Console.WriteLine($"\t\t{member.Name}");
+        
+        if (member is MethodInfo method)
+        {
+            Console.WriteLine("\t\t\tReturn type:");
+            Console.WriteLine($"\t\t\t\t{method.ReturnType.Name}");
+
+            var methodParameters = method.GetParameters();
+            foreach (var methodParameter in methodParameters)
+            {
+                Console.WriteLine($"\t\t\t\t{methodParameter.Name} : {methodParameter.ParameterType.Name}");
+
+                if (methodParameter.ParameterType.IsGenericType)
+                {
+                    var constraints = methodParameter.ParameterType.GetGenericParameterConstraints();
+                    if (constraints.Length > 0)
+                    {
+                        Console.WriteLine("\t\t\t\t\tConstraints:");
+                        foreach (var constraint in constraints)
+                        {
+                            Console.WriteLine($"\t\t\t\t\t\t must be {constraint.Name}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Reflection/Generics/Types.cs
@@ -0,0 +1,42 @@
+﻿namespace Generics;
+
+public sealed class SomeImplementation(string input) : ISomeInterface
+{
+    public void SomeMethod()
+    {
+        Console.WriteLine($"SomeMethod({input})");
+    }
+
+    public override string ToString()
+    {
+        return input;
+    }
+}
+
+public sealed class  SomeOtherImplementation(int input) : ISomeOtherInterface
+{
+    public override string ToString()
+    {
+        return input.ToString();
+    }
+}
+
+public interface ISomeOtherInterface;
+
+public interface ISomeInterface
+{
+    public void SomeMethod();
+}
+
+public class GenericTypeA<T1, T2, T3> where T2 : ISomeInterface
+{
+    public void Method1(T1 value1, T2 value2)
+    {
+        Console.WriteLine($"Method1<{typeof(T1).Name}, {typeof(T2).Name}>({value1}, {value2})");
+    }
+
+    public void Method2<TMethodParameter>(TMethodParameter parameter) where TMethodParameter : ISomeInterface
+    {
+        Console.WriteLine($"Method2<{typeof(TMethodParameter).Name}>({parameter})");
+    }
+}
