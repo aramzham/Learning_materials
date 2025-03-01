4d5daeec65df85c8ddaae138a8cdf2740c648ae3
Aram Zhamkochyan <aramzham@hotmail.com>
2025-03-01 10:40:07 +0400

    reflection: creating generic types

--- Reflection/Generics/CallingGenericMethods.cs
+++ Reflection/Generics/CallingGenericMethods.cs
@@ -9,7 +9,7 @@ public class CallingGenericMethods
         var genericTypeA = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsGenericType)
             .SingleOrDefault(t => t.Name == "GenericTypeA`3");
         var genericMethods = genericTypeA.GetMethods().Where(x => x.IsGenericMethod).ToArray();
-
+        
         var instance = new GenericTypeA<int, SomeImplementation, string>();
 
         var method1 = instance.GetType().GetMethod("Method1");
@@ -19,5 +19,9 @@ public class CallingGenericMethods
         // method2.Invoke(instance, [new SomeOtherImplementation(124)]); // we can't do such late binding because at runtime we don't know the generic type parameter of the method
         method2 = method2.MakeGenericMethod(typeof(SomeImplementation));
         method2.Invoke(instance, [new SomeImplementation("input_2")]);
+
+        genericTypeA = genericTypeA.MakeGenericType(typeof(string), typeof(SomeImplementation), typeof(int));
+        var instance2 = (GenericTypeA<string, SomeImplementation, int>)Activator.CreateInstance(genericTypeA)!;
+        instance2.Method1("input_3", new SomeImplementation("input_4"));
     }
 }
