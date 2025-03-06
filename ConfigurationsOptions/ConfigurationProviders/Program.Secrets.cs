2ea7c70927b1a1b62190757eea445cb63048435c
Aram Zhamkochyan <aramzham@hotmail.com>
2025-03-06 09:01:10 +0400

    configsOptions: user secrets

---
+++ ConfigurationsOptions/ConfigurationProviders/Program.Secrets.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration.UserSecrets;
+
+// represents user secrets id
+// in most cases, this attribute is automatically generated during compilation by MSBuild targets included in the UserSecrets NuGet package.
+// these targets use the MSBuild property 'UserSecretsId' to set this value
+
+// [assembly: UserSecretsId("019569c6-979a-78be-b38e-304194ff3771")]
+//
+// internal partial class Program
+// {
+//     // you don't need to this manually
+// }
---
+++ ConfigurationsOptions/ConfigurationProviders/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateEmptyApplicationBuilder(null);
+
+// JSON {;}
+builder.Configuration.AddJsonFile(
+    path: "appsettings.json",
+    optional: false,
+    reloadOnChange: true);
+
+builder.Configuration.AddJsonFile(
+    path: $"appsettings.{builder.Environment.EnvironmentName}.json",
+    optional: true,
+    reloadOnChange: true);
+
+// XML </>
+
+builder.Configuration.AddXmlFile(
+    path: "config.xml",
+    optional: true,
+    reloadOnChange: true
+    );
+    
+builder.Configuration.AddXmlFile(
+    path: $"config.{builder.Environment.EnvironmentName}.xml",
+    optional: true,
+    reloadOnChange: true
+);
+
+// INI [=]
+builder.Configuration.AddIniFile(
+    path: "app.ini",
+    optional: true,
+    reloadOnChange: true
+    );
+builder.Configuration.AddIniFile(
+    path: $"app.{builder.Environment.EnvironmentName}.ini",
+    optional: true,
+    reloadOnChange: true
+);
+
+// user secrets (?!)
+builder.Configuration.AddUserSecrets<Program>(
+    optional: false,
+    reloadOnChange: true
+    );
+
+Console.WriteLine(builder.Configuration.GetDebugView());
