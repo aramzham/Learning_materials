1f412998801edaf61ef891a8f7af6a94a96584b0
Aram Zhamkochyan <aramzham@hotmail.com>
2025-11-04 09:05:34 +0400

    cache: Fusion vs Hybrid

---
+++ Cache/ParallelGet/HybridCacheExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Caching.Hybrid;
+
+namespace ParallelGet;
+
+public static class HybridCacheExtensionMethods
+{
+    private static readonly HybridCacheEntryOptions _TryGetAsyncEntryOptions = new()
+        { Flags = HybridCacheEntryFlags.DisableLocalCacheWrite | HybridCacheEntryFlags.DisableDistributedCacheWrite };
+
+    // problems:
+    // - non deterministic
+    // - wrong behavior with L2 to L1 copy
+    public static async ValueTask<(bool Found, T? Value)> TryGetAsync<T>(this HybridCache cache, string key)
+    {
+        var found = true;
+        var value = await cache.GetOrCreateAsync<T>(key, _ =>
+        {
+            found = false;
+            return ValueTask.FromResult(default(T))!;
+        }, _TryGetAsyncEntryOptions);
+
+        return (found, value);
+    }
+}
---
+++ Cache/ParallelGet/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Caching.Hybrid;
+using Microsoft.Extensions.DependencyInjection;
+using ParallelGet;
+using ZiggyCreatures.Caching.Fusion;
+
+// setup di
+var services = new ServiceCollection();
+services.AddHybridCache();
+// services.AddFusionCache().AsHybridCache(); // magic
+
+// get the cache
+var serviceProvider = services.BuildServiceProvider();
+var cache = serviceProvider.GetRequiredService<HybridCache>();
+
+// test (sequential)
+Console.WriteLine("Sequential");
+for (int i = 0; i < 10; i++)
+{
+    var value = await cache.TryGetAsync<int>("test");
+    Console.WriteLine($"value = {value.Value}, found = {value.Found.ToString().ToUpper()}");
+}
+Console.WriteLine("Done");
+
+// test (parallel)
+Console.WriteLine("Parallel");
+await Parallel.ForEachAsync(Enumerable.Range(0, 10), async (_, _) =>
+{
+    var value = await cache.TryGetAsync<int>("test");
+    Console.WriteLine($"value = {value.Value}, found = {value.Found.ToString().ToUpper()}");
+});
