03b5e4a831438c95f34d9e23f693874a1b911799
Aram Zhamkochyan <aramzham@hotmail.com>
2026-03-04 08:55:17 +0400

    pp initial

---
+++ Parallel programming/Deadlocks/Program.cs
@@ -0,0 +1,19 @@
+﻿#region Deadlock!!
+
+var bgThread1 = Task.Run(() => Thread.Sleep(100));
+var bgThread2 = Task.Run(() => Thread.Sleep(200));
+
+bgThread1 = Task.Run(() => SimulateLongRunningTask(bgThread2, CancellationToken.None));
+bgThread2 = Task.Run(() => SimulateLongRunningTask(bgThread1, CancellationToken.None));
+
+await bgThread1;
+await bgThread2;
+
+async Task SimulateLongRunningTask(Task otherLongRunningFunction, CancellationToken token)
+{
+    await Task.Delay(TimeSpan.FromSeconds(2), token);
+    await otherLongRunningFunction;
+    Console.WriteLine($"Finished thread {Thread.CurrentThread.ManagedThreadId}");
+}
+
+#endregion
---
+++ Parallel programming/IntroToRaceConditions/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+bool result = false;
+int loopIterations = 0;
+
+Trace.WriteLine($"Program started on thread {Environment.CurrentManagedThreadId}");
+
+while (loopIterations < 1_000)
+{
+    var bgTask1 = Task.Run(() => SimulateLongRunningTask("Task-1", true, CancellationToken.None));
+    var bgTask2 = Task.Run(() => SimulateLongRunningTask("Task-2", false, CancellationToken.None));
+
+    await bgTask1;
+    await bgTask2;
+    
+    loopIterations++;
+}
+
+async Task SimulateLongRunningTask(string name, bool setResult, CancellationToken cancellationToken)
+{
+    Trace.WriteLine($"{name} started on thread {Environment.CurrentManagedThreadId}");
+    
+    await Task.Delay(2_000);
+
+    result = setResult;
+    
+    Trace.WriteLine($"Set {nameof(result)} to {setResult}");
+
+    if (result != setResult)
+    {
+        throw new Exception("Race condition detected!!");
+    }
+
+    Console.WriteLine($"{name} completed on thread {Environment.CurrentManagedThreadId}!!");
+}
