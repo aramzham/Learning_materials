f1973c04c2f197e0374377ed97422387da746ef6
Aram Zhamkochyan <aramzham@hotmail.com>
2026-03-09 08:52:04 +0400

    pp: parallel invoke

---
+++ Parallel programming/ParallelInvoke/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using ParallelInvoke;
+
+Console.WriteLine("Hello, World!");
+
+var salad = new Salad();
+var risotto = new Risotto();
+var pizza = new Pizza();
+var lasagna = new Lasagna();
+var pasta = new Pasta();
+
+var cts = new CancellationTokenSource(TimeSpan.FromSeconds(40));
+var parallelOptions = new ParallelOptions()
+{
+    CancellationToken = cts.Token,
+    MaxDegreeOfParallelism = 2
+};
+
+Trace.WriteLine("Starting to cook.");
+
+try
+{
+    Parallel.Invoke(parallelOptions, 
+        () => salad.Cook(), 
+        risotto.Cook, 
+        pizza.Cook, 
+        lasagna.Cook, 
+        pasta.Cook);
+}
+catch (OperationCanceledException e)
+{
+    Trace.WriteLine(e.Message);
+    Trace.WriteLine("Some dishes might be missing.");
+}
+
+Trace.WriteLine("Done.");
+
