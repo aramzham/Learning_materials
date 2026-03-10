27f0e845df1d849b6c8ea27ff1a49aa9e005333f
Aram Zhamkochyan <aramzham@hotmail.com>
2026-03-10 09:06:17 +0400

    pp: parallel for and async

---
+++ Parallel programming/ParallelFor/Food.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace ParallelFor;
+
+public abstract class Food
+{
+    private TimeSpan _cookingTime;
+    
+    public string Name { get; }
+
+    protected Food(TimeSpan cookingTime)
+    {
+        _cookingTime = cookingTime;
+        Name = this.GetType().Name;
+    }
+    
+    public async Task Cook(int orderNumber, CancellationToken token)
+    {
+        Trace.WriteLine($"Cooking {Name} for order {orderNumber} on {Environment.CurrentManagedThreadId}");
+        await Task.Delay(_cookingTime, token);
+        Trace.WriteLine($"{Name} completed for order {orderNumber} on {Environment.CurrentManagedThreadId}!");
+    }
+    
+    public override string ToString() => Name;
+}
+
+public class Risotto() : Food(TimeSpan.FromSeconds(5));
+public class Salad() : Food(TimeSpan.FromSeconds(2));
+public class Pasta() : Food(TimeSpan.FromSeconds(3));
+public class Pizza() : Food(TimeSpan.FromSeconds(8));
+public class Lasagna() : Food(TimeSpan.FromSeconds(10));
---
+++ Parallel programming/ParallelFor/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using ParallelFor;
+
+Console.WriteLine("Hello, World!");
+
+var salad = new Salad();
+var risotto = new Risotto();
+var pizza = new Pizza();
+var lasagna = new Lasagna();
+var pasta = new Pasta();
+
+const int numOfOrdersSalad = 10;
+const int numOfOrdersRisotto = 3;
+const int numOfOrdersPizza = 5;
+const int numOfOrdersLasagna = 2;
+const int numOfOrdersPasta = 1;
+
+var cts = new CancellationTokenSource(TimeSpan.FromSeconds(40));
+var parallelOptions = new ParallelOptions()
+{
+    CancellationToken = cts.Token,
+    MaxDegreeOfParallelism = 50
+};
+
+Trace.WriteLine("Starting to cook.");
+
+try
+{
+    var saladCookingTask = Parallel.ForAsync(0, numOfOrdersSalad, parallelOptions, async (orderNumber, token) => await salad.Cook(orderNumber, token));
+    var risottoCookingTask = Parallel.ForAsync(0, numOfOrdersRisotto, parallelOptions, async (orderNumber, token) => await risotto.Cook(orderNumber, token));
+    var pizzaCookingTask = Parallel.ForAsync(0, numOfOrdersPizza, parallelOptions,
+        async (orderNumber, token) => await pizza.Cook(orderNumber, token));
+    var lasagnaCookingTask = Parallel.ForAsync(0, numOfOrdersLasagna, parallelOptions, async (orderNumber, token) => await lasagna.Cook(orderNumber, token));
+    var pastaCookingTask = Parallel.ForAsync(0, numOfOrdersPasta, parallelOptions, async (orderNumber, token) => await pasta.Cook(orderNumber, token));
+    
+    await Task.WhenAll(saladCookingTask, risottoCookingTask, pizzaCookingTask, lasagnaCookingTask, pastaCookingTask);
+}
+catch (OperationCanceledException e)
+{
+    Trace.WriteLine(e.Message);
+    Trace.WriteLine("Some dishes might be missing.");
+}
+
+Trace.WriteLine("Done.");
+
