39595ba10d311a1c3aba2cfdefaccb893b098ae7
Aram Zhamkochyan <aramzham@hotmail.com>
2026-03-14 11:50:29 +0400

    pp: channels

---
+++ Parallel programming/Channels/Food.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace Channels;
+
+public abstract class Food
+{
+    private readonly TimeSpan _cookingTime;
+    
+    public string Name { get; }
+
+    protected Food(TimeSpan cookingTime)
+    {
+        _cookingTime = cookingTime;
+        Name = GetType().Name;
+    }
+    
+    public async Task<string> Cook(CancellationToken ct)
+    {
+        Trace.WriteLine($"Start cooking {Name}");
+        await Task.Delay(_cookingTime, ct);
+        Trace.WriteLine($"{Name} is ready!");
+        
+        return Name;
+    }
+    
+    public override string ToString() => Name;
+}
+
+public class Risotto() : Food(TimeSpan.FromSeconds(5));
+public class Salad() : Food(TimeSpan.FromSeconds(2));
+public class Pasta() : Food(TimeSpan.FromSeconds(3));
+public class Pizza() : Food(TimeSpan.FromSeconds(8));
+public class Lasagna() : Food(TimeSpan.FromSeconds(10));
---
+++ Parallel programming/Channels/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using Channels;
+
+Console.WriteLine("Hello, World!");
+
+const int numTables = 5;
+
+var restaurantOpenDuration = TimeSpan.FromSeconds(10);
+
+IReadOnlyList<Food> menu = [
+    new Lasagna(),
+    new Lasagna(),
+    new Salad(),
+    new Pizza(),
+    new Pizza(),
+    new Risotto(),
+    new Pasta()
+];
+
+var options = new BoundedChannelOptions(numTables)
+{
+    AllowSynchronousContinuations = true,
+    SingleReader = true,
+    SingleWriter = false,
+    FullMode = BoundedChannelFullMode.Wait
+};
+var orderChannel = Channel.CreateBounded<Food>(options);
+
+Console.WriteLine("Restaurant Opened");
+var restaurantOpenedTime = DateTimeOffset.UtcNow;
+
+var kitchenTask = KitchenTask();
+
+while (restaurantOpenedTime + restaurantOpenDuration > DateTimeOffset.UtcNow)
+{
+    // take order 
+    var food = GetOrder();
+    
+    var numSecondsBeforeNextOrder = Random.Shared.Next(1, 3);
+    await orderChannel.Writer.WriteAsync(food);
+
+    Console.WriteLine();
+    Console.WriteLine($"Submitting order for {food.Name}. Total unread orders: {orderChannel.Reader.Count}");
+    Console.WriteLine();
+    
+    // wait for next order
+    await Task.Delay(TimeSpan.FromSeconds(numSecondsBeforeNextOrder));
+}
+
+Console.WriteLine();
+Console.WriteLine($"Restaurant closing with {orderChannel.Reader.Count} orders remaining to cook in the kitchen");
+Console.WriteLine();
+
+_ = orderChannel.Writer.TryComplete();
+
+await kitchenTask;
+
+Console.WriteLine();
+Console.WriteLine("Kitchen closed");
+Console.WriteLine();
+return;
+
+Food GetOrder()
+{
+    var random = Random.Shared.Next(0, menu.Count);
+    return menu[random];
+}
+
+async Task KitchenTask(CancellationToken ct = default)
+{
+    await foreach (var item in orderChannel.Reader.ReadAllAsync(ct))
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Kitchen: Cooking {item.Name}");
+        Console.WriteLine();
+        await item.Cook(ct);
+    }
+}
